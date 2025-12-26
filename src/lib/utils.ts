a1fe7e102f6e43b4cd3faee31cd8151679e5d001
devkarim <devkarim@hotmail.com>
2025-12-26 22:08:15 +0200

    feat: add utility helper functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { UserType } from './types/auth';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function capitalize(str: string): string {
+  return str
+    .split(/[\s_]+/)
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+}
+
+export function getHomeRoute(currentType: UserType): string {
+  return currentType === UserType.FREELANCER ? '/app/find-work' : '/app/hire-talent';
 }
